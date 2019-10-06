c23f9195c52f339c2aa5270b00d77cceb11bfe18
Muhammad Khizar <khizaransari721@gmail.com>
2019-10-05 18:06:54 -0700

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'products'], function () {
+
+    Route::apiResource('Products', 'ProductController');
+
+});
+
+
